98e8d708d3d1b1f7a5d7ebb6722486dd04b47bd9
Bruno de Araújo Alves <me@baraus.dev>
2022-05-03 11:23:24 -0300

    Initial

---
+++ main.js
@@ -0,0 +1,15 @@
+import { getAudioData, linearPath, polarPath } from "./waveform-path.min.js";
+
+async function AudioPath(file) {
+  const audioData = await getAudioData(file);
+
+  const pathLogo = linearPath(audioData, {
+    samples: 120,
+    type: "mirror",
+    top: 40,
+    paths: [{ d: "V", sy: 0, x: 50, ey: 100 }],
+  });
+  document.querySelector("#path_whole").setAttribute("d", pathLogo);
+  document.querySelector("#path_mask").setAttribute("d", pathLogo);
+}
+AudioPath("0BxE4FqsDD1Ot4YuBXwAPp.mp3");
